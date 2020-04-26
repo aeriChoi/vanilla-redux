54dc14bd6b41d0114998013dac515e1b21605e3f
aeri choi <aeri.choi715@gmail.com>
2020-04-26 22:44:45 +0900

    feat(master): add detai pagesl

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,24 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+
+function Detail({ toDo }) {
+  return (
+    <>
+      <h3>No. {toDo?.id}</h3>
+      <h1>{toDo?.text}</h1>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: {id}
+    }
+  } = ownProps;
+  console.log(id);
+  console.log(state);
+  return { toDo: state.find(toDo => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
